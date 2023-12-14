6d489d246f87c33da49d5402cf25cf2e3c3e87c0
sah_314 <khadayatesahil@gmail.com>
2023-12-14 15:47:22 +0530

    feat: added dropdown button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useEffect} from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+
+useEffect(() => {
+  
+
+  return () => {
+    
+  }
+}, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <div className="App" >
+   <details className="dropdown fixed top-80 right-1/2">
+  <summary className="m-1 btn border-none text-[#63BAAB] text-center ml-10">open or close</summary>
+  <ul className="p-2 shadow menu dropdown-content z-[1] rounded-box w-52 text-[#63BAAB] bg-slate-800 mt-3 mr-3 pr-3">
+    <button>get top 10 repos</button>
+    <button>get top 50 repos</button>
+    <button>get top 100 repos</button>
+  </ul>
+</details>
+   </div>
+   </>
   );
 }
 
